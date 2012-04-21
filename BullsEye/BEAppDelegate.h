b16084d140d6b6af50bbf28d28ac9d44a6122a3a
Ashok Gelal <ashokgelal@gmail.com>
2012-04-21 00:01:44 -0600

    Initial Commit

---
+++ BullsEye/BEAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  BEAppDelegate.h
+//  BullsEye
+//
+//  Created by Ashok Gelal on 4/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BEViewController;
+
+@interface BEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) BEViewController *viewController;
+
+@end
---
+++ BullsEye/BEViewController.h
@@ -0,0 +1,13 @@
+//
+//  BEViewController.h
+//  BullsEye
+//
+//  Created by Ashok Gelal on 4/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BEViewController : UIViewController
+
+@end

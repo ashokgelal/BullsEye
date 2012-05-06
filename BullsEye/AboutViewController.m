a464b00825fa3dafa1a04cf8da185dd47f94753b
Ashok Gelal <ashokgelal@gmail.com>
2012-05-06 13:18:55 -0600

    Added missing files. Ready for final push

---
+++ BullsEye/AboutViewController.m
@@ -0,0 +1,48 @@
+//
+//  AboutViewController.m
+//  BullsEye
+//
+//  Created by Ashok Gelal on 4/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AboutViewController.h"
+
+@implementation AboutViewController
+@synthesize webView;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    NSString *htmlFile = [[NSBundle mainBundle] pathForResource:@"BullsEye" ofType:@"html"];
+    NSData *htmlData = [NSData dataWithContentsOfFile:htmlFile];
+    NSURL *baseUrl = [NSURL fileURLWithPath:[[NSBundle mainBundle] bundlePath]];
+    
+    [self.webView loadData:htmlData MIMEType:@"text/html" textEncodingName:@"UTF-8" baseURL:baseUrl];
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    self.webView = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return UIInterfaceOrientationIsLandscape(interfaceOrientation);
+}
+
+-(IBAction)close
+{
+    [self.presentingViewController dismissViewControllerAnimated:YES completion:nil];
+}
+@end
--- BullsEye/BEViewController.h
+++ BullsEye/BEViewController.h
@@ -8,6 +8,17 @@
 
 #import <UIKit/UIKit.h>
 
-@interface BEViewController : UIViewController
+@interface BEViewController : UIViewController<UIAlertViewDelegate>
+@property (nonatomic, strong) IBOutlet UISlider *slider;
+@property (nonatomic, strong) IBOutlet UILabel *targetLabel;
+@property (nonatomic, strong) IBOutlet UILabel *scoreLabel;
+@property (nonatomic, strong) IBOutlet UILabel *roundLabel;
+
+
+- (IBAction)showAlert;
+- (IBAction)sliderMoved:(UISlider *)slider;
+- (IBAction)startOver;
+- (IBAction)showInfo;
+
 
 @end
